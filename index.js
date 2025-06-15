7d19de21e3e96bcae596d4820f3b6de17dfa88c0
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-06-15 17:15:26 +0600

    server url changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Connection();
 
 // middlewares
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'https://typetogetherserver.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
 }));
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 // Socket.IO setup
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        origin: ['http://localhost:5173'],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 });
