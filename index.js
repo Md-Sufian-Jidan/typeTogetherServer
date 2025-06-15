af45618d21f4b6842974cfc6f5c6b42acfb3d8ee
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-06-15 18:17:07 +0600

    server redeploy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Connection();
 
 // middlewares
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://typetogether-six.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
 }));
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 // Socket.IO setup
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://typetogether-six.vercel.app/'],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 });
